04d7ce24e651e274788ad3fb89ba9144ff53a2f1
geoffreyoliver <geoffreyokongo2@gmail.com>
2016-07-31 22:00:04 +0300

    Creating the controller and method for loading the presentation creation interface

--- application/controllers/Presentation.php
+++ application/controllers/Presentation.php
@@ -7,7 +7,7 @@ class Presentation extends CI_Controller {
 	 * This controller method loads a presentation creation interface
 	 *
 	 */
-	public function create()
+	public function index()
 	{
 		$this->load->helper('url');
 		$this->load->view('presentation/create');
